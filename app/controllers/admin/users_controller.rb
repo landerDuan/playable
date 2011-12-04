60136a92266991cfb36907a91ba92b5af0f32d6e
JerryShen <hansay99@gmail.com>
2011-12-04 12:19:44 +0800

    add user management

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,3 +1,9 @@
 class Admin::UsersController < Admin::BaseController
   
+  protected
+  
+  def collection
+    @users ||= end_of_association_chain.page(params[:page])
+  end
+  
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def i18n_for_new_link(resource_class)
+    t('admin.actions.new', :model => t('activerecord.models.' + resource_class.downcase))
+  end
+  alias :tt :i18n_for_new_link
 end
