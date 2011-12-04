e5c288c8b0c00700b426f6578d3a55c1fef7d66c
landerpc <duan@playab.net>
2011-12-04 10:56:30 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Playable::Application.config.secret_token = 'daea1592d3c5da04a98de56275d7333560f17da4a7d6850ad2ef7ddc02829da6a7b73108b2782d7b7b7eafc9021fb95d14e8b6ca68da5a3843f2f5f99236418f'
