2e0b0366819b2887f4be44219a4c63bd69f1d5d6
lip <li@playab.net>
2011-12-06 18:40:33 +0800

    mark search commit

--- app/controllers/admin/marks_controller.rb
+++ app/controllers/admin/marks_controller.rb
@@ -2,34 +2,68 @@ class Admin::MarksController < Admin::BaseController
   main_nav_highlight :marks
   
   def checkin
-    @user_id = 2
+    @user_id = current_user[:id]
     @checkin_at = Time.now
-    
-    @mark = Mark.new()
-    @mark.user_id = @user_id
-    @mark.checkin_at = @checkin_at
-    @mark.save
-    redirect_to admin_marks_path
+   
+    @today = @checkin_at.to_time.to_s[0,10]
+    params[:user_id] = @user_id
+    params[:checkin_at] = @today
+    @today_checkin_info = Mark.find(:first, :conditions => ["user_id = :user_id AND checkin_at >= :checkin_at",params])
+    if @today_checkin_info
+      redirect_to admin_marks_path
+    else 
+      @mark = Mark.new()
+      @mark.user_id = @user_id
+      @mark.checkin_at = @checkin_at
+      @mark.save
+      redirect_to admin_marks_path
+    end
   end
   
   def checkout
-    @user_id = 2
+    @user_id = current_user[:id]
     @checkout_at = Time.now
-    
-    
     @today = @checkout_at.to_time.to_s[0,10]
     
     params[:user_id] = @user_id
     params[:checkin_at] = @today
-    @today_checkin_info = Mark.find(:first, :conditions => ["user_id = :user_id and checkin_at >= :checkin_at",params])
+    @today_checkin_info = Mark.find(:first, :conditions => ["user_id = :user_id AND checkin_at >= :checkin_at",params])
     if @today_checkin_info[:checkout_at]
-      #redirect_to admin_marks_path
+      redirect_to admin_marks_path
+    else
+      Mark.where('id = ?', @today_checkin_info[:id]).update_all(:checkout_at => @checkout_at)
+      redirect_to admin_marks_path
     end
-    Mark.where('id = ?', @today_checkin_info[:id]).update_all(:checkout_at => @checkout_at)
-
-    redirect_to admin_marks_path
     #转化成时间戳
     #Time.now.to_i
+  end
+  
+  def index
+    @mark = Mark.new
+    @user_id = current_user[:id]
+    @today = Time.now.to_time.to_s[0,10]
+    @search_user = params[:mark]
+    if @search_user
+      @user = User.where("username = '" << @search_user[:user_id] << "'")
+      params[:user_id] = @user[0]['id']
+      @all_marks = Mark.find(:all, :conditions => ["user_id = :user_id",params], :limit => 1)
+    else
+      params[:user_id] = @user_id
+      params[:checkin_at] = @today
+      @today_check_info = Mark.find(:first, :conditions => ["user_id = :user_id AND checkin_at >= :checkin_at",params])
+      @all_marks = Mark.all(:order => "id ASC")
+    end
+    
+  end
+  
+  def search
+    @user_name = params[:mark][:user_id]
+    @user = User.where("username = '" << @user_name << "'")
+    #@user = User.first
+    params[:user_id] = @user[0]['id']
+    render :text => @user[0]['id']
+    @all_marks = Mark.find(:all, :conditions => ["user_id = :user_id",params],:limit => 1)
+    #redirect_to admin_marks_path
   end
   
   protected
