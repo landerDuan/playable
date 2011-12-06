c34be8f0d984f8b2a9d64b2edb56d0c5488f5788
suncx <suncx@playab.net>
2011-12-06 18:38:49 +0800

    reports chang

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -4,11 +4,19 @@ class Admin::ReportsController < Admin::BaseController
   def show
     redirect_to admin_reports_path
   end
+  def index
+ 		@uid = params[:uid]
+  	if !@uid
+  		@uid = current_user[:id]
+  	end
+  	@reports = Report.where('user_id=' << @uid.to_s)
+  	@all_user = User.all
+  end
   
   protected
   
   def collection
-    @users ||= end_of_association_chain.page(params[:page])
+     @users ||= end_of_association_chain.page(params[:page])
   end
   
 end
