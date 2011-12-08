b7f6701293ff1b4c7e2ae7b043cf3293777da8ba
lip <li@playab.net>
2011-12-08 18:17:27 +0800

    mark search commit

--- app/controllers/admin/marks_controller.rb
+++ app/controllers/admin/marks_controller.rb
@@ -31,51 +31,22 @@ class Admin::MarksController < Admin::BaseController
     if @today_checkin_info[:checkout_at]
       redirect_to admin_marks_path
     else
-      Mark.where('id = ?', @today_checkin_info[:id]).update_all(:checkout_at => @checkout_at)
+      Mark.where('id = ?', @today_checkin_info[:id]).update_attibutes(:checkout_at => @checkout_at)
       redirect_to admin_marks_path
     end
-    #转化成时间戳
-    #Time.now.to_i
-  end
-  
-  def index
-    @mark = Mark.new
-    @user_id = current_user[:id]
-    @today = Time.now.to_time.to_s[0,10]
-    @search_user = params[:mark]
-    if @search_user
-      #@user = User.where("username = '" << @search_user[:user_id] << "'")
-      #params[:user_id] = @user[0]['id']
-      params[:user_id] = @search_user[:user_id]
-      @all_marks = Mark.find(:all, :conditions => ["user_id = :user_id",params], :limit => 1)
-    else
-      params[:user_id] = @user_id
-      params[:checkin_at] = @today
-      @today_check_info = Mark.find(:first, :conditions => ["user_id = :user_id AND checkin_at >= :checkin_at",params])
-      @all_marks = Mark.all(:order => "id ASC")
-    end
-    
-  end
-  
-  def search
-    @user_name = params[:mark][:user_id]
-    @user = User.where("username = '" << @user_name << "'")
-    #@user = User.first
-    params[:user_id] = @user[0]['id']
-    render :text => @user[0]['id']
-    @all_marks = Mark.find(:all, :conditions => ["user_id = :user_id",params],:limit => 1)
-    #redirect_to admin_marks_path
-  end
-  
-  def show
-    redirect_to admin_marks_path
   end
   
   protected
   
   def collection
-    @marks ||= end_of_association_chain.page(params[:page])
+    if params.key?(:user_id)
+      @user = User.find(params[:user_id])
+      @search = @user.marks.search(params[:search])
+    else
+      @search ||= end_of_association_chain.search(params[:search])
+    end
+    
+    @marks = @search.page(params[:page]) 
   end
-  
 
 end
