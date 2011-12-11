95c5657d8bf74339e0473ab1d711304af0f7966d
suncx <suncx@playab.net>
2011-12-11 16:36:45 +0800

    product commit

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,4 @@
 class Product < ActiveRecord::Base
 	has_many :photos, :class_name => 'ProductPhoto'
-	
 	accepts_nested_attributes_for :photos, :allow_destroy => true, :reject_if => :all_blank
 end
