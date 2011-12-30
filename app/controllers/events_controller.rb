24ea2fdf1a4bec45fda24d1003d148efb27570f9
suncx <suncx@playab.net>
2011-12-30 12:05:15 +0800

    commit

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,21 +3,37 @@ class EventsController < ApplicationController
 
   def index
   	@event = current_user.events.order("id desc").first
-    @events = current_user.events.page(params[:page])
+    @events = current_user.events.order("id desc").page(params[:page])
   end
 
   def check_in
-  	@event = Event.new
-    @event.user_id = current_user[:id]
-    @event.checkin_at = Time.now
-    @event.save
-  	redirect_to events_path
+    @eventInfo = current_user.events.order("id desc").first
+    if @eventInfo.blank?
+      @event = Event.new
+      @event.user_id = current_user[:id]
+      @event.checkin_at = Time.now
+      @event.save
+      redirect_to events_path
+    else
+      if @eventInfo.checkin_at > Time.now.to_date
+        redirect_to events_path
+      else
+      	@event = Event.new
+        @event.user_id = current_user[:id]
+        @event.checkin_at = Time.now
+        @event.save
+    	  redirect_to events_path
+      end
+    end
   end
 
   def check_out
-  	@event = current_user.events.last
-  	@event.checkout_at = Time.now
-  	@event.save
-  	redirect_to events_path
+    @event = current_user.events.last
+    if @event.checkout_at.blank?
+    	@event.checkout_at = Time.now
+    	@event.save
+    end
+    redirect_to events_path
   end
+
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,13 @@
 class Event < ActiveRecord::Base
 	belongs_to :user
 
+	def check_in
+		@event = self.new
+    @event.user_id = current_user[:id]
+    @event.checkin_at = Time.now
+    @event.save
+    redirect_to events_path
+	end
 end
 # == Schema Information
 #
