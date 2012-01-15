4dd1f96891742d34c9f4cc7ecf56a4fdbf3bebab
JerryShen <hansay99@gmail.com>
2012-01-15 12:25:09 +0800

    fix bug for popover link to helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,14 +1,12 @@
 module UsersHelper
   
-  def popover_link_to(text, url_options = {}, content = {}, html_options = {})
+  def popover_link_to(text, url_options = {}, html_options = {})
     html_options.merge!(
       :'data-placement' => 'below',
       :rel => 'popover',
-      :'data-content' => content[:content],
-      :'data-original-title' => content[:title],
       :class => 'should_popover'
     )
     
-    link_to(text, url_options, content, html_options)
+    link_to(text, url_options, html_options)
   end
 end
