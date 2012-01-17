dd90045380b5b0c918885bb5a40ae472e54eeef7
suncx <suncx@playab.net>
2012-01-17 17:45:34 +0800

    add profile report_edit

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,7 +5,6 @@ class EventsController < ApplicationController
   protected
   
   def collection
-    # @events ||= current_user.events.group_by{ |x| x.created_at.beginning_of_week }
-    @events ||= Event.scoped.group_by{ |x| x.checkin_at.beginning_of_week }
+    @events ||= current_user.events.group_by{ |x| x.created_at.beginning_of_week }
   end
 end
